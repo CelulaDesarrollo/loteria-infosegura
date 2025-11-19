196ee9a00fa83db3428bba27177689d8cea2c6b1
CelulaDesarrollo <celulacontenidos@uv.mx>
2025-11-19 15:23:54 -0600

    nueva url admitida por el CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,6 +20,7 @@ async function startServer() {
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:9002",
+    "http://148.226.24.22",
   ];
 
   const allowedOrigins = new Set<string>([PROD_CLIENT, DEV_CLIENT, ADMIN_CLIENT, ...EXTRA_DEV]);
